7bd4635589539573dc214f2c1e02b29b9b15a5d9
Sorway <sorwayhd@gmail.com>
2024-05-09 00:18:17 +0200

    Updating build.gradle.kts and adding .gitignore file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     id("java")
+    id("maven-publish")
 }
 
 group = "fr.sorway"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -14,6 +15,29 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/Niobium-MC/API")
+            credentials {
+                username = project.findProject("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProject("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+
+    publications {
+        register("jar", MavenPublication::class) {
+            artifactId = "niobium-api"
+            from(components["java"])
+            pom {
+                url.set("https://github.com/Niobium-MC/API.git")
+            }
+        }
+    }
 }
 
 java {
