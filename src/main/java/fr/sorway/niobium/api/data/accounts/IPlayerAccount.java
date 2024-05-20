1a570918adf6f8c987d9b1def0debf1eeb53f02b
Sorway <sorwayhd@gmail.com>
2024-05-20 02:20:33 +0200

    Changing IPlayerAccount.java

--- src/main/java/fr/sorway/niobium/api/data/accounts/IPlayerAccount.java
+++ src/main/java/fr/sorway/niobium/api/data/accounts/IPlayerAccount.java
@@ -2,11 +2,9 @@ package fr.sorway.niobium.api.data.accounts;
 
 import fr.sorway.niobium.api.data.accounts.data.IPlayerRank;
 
-import java.net.Inet4Address;
-
 public interface IPlayerAccount {
     boolean isNewPlayer();
-    Inet4Address getIpAddress();
+    String getIpAddress();
     long getShopMoney();
     long getCreatedAt();
     long getUpdatedAt();
