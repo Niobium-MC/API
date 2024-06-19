d6a86928b0499b35794d9437c609a9636ee7c566
Sorway <sorwayhd@gmail.com>
2024-06-19 22:35:00 +0200

    📚 Adding JavaDocs

--- src/main/java/fr/sorway/niobium/api/managers/data/account/IRankAccountManager.java
+++ src/main/java/fr/sorway/niobium/api/managers/data/account/IRankAccountManager.java
@@ -14,6 +14,24 @@ public interface IRankAccountManager {
     void assignRank(IPlayerAccount account, IAccountRank rank);
 
     /**
+     * Removes the specified rank from the given player account.
+     *
+     * @param account the player account from which the rank will be removed.
+     *                Must not be null.
+     * @param rank the rank to be removed from the player account.
+     *             Must not be null.
+     */
+    void unassignRank(IPlayerAccount account, IAccountRank rank);
+
+    /**
+     * Adds a rank to the specified player account.
+     *
+     * @param account the player account to which the rank will be added
+     * @param rank the rank to be added to the player's account
+     */
+    void addRank(IPlayerAccount account, IAccountRank rank);
+
+    /**
      * Removes a specific rank from a player's account.
      *
      * @param account the player account from which the rank will be removed.
