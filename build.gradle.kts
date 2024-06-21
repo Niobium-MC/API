b753e6b1ffaa27a2d42e1c7613ab6c58c64b8039
Sorway <sorwayhd@gmail.com>
2024-06-21 23:35:26 +0200

    📚 Adding JavaDocs

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "fr.sorway.niobium"
-version = "1.0-beta.8"
+version = "1.0-beta.9"
 
 repositories {
     mavenCentral()
--- src/main/java/fr/sorway/niobium/api/data/accounts/IPlayerAccount.java
+++ src/main/java/fr/sorway/niobium/api/data/accounts/IPlayerAccount.java
@@ -2,6 +2,8 @@ package fr.sorway.niobium.api.data.accounts;
 
 import fr.sorway.niobium.api.data.accounts.data.IPlayerRank;
 
+import java.util.UUID;
+
 public interface IPlayerAccount {
 
     /**
@@ -15,6 +17,24 @@ public interface IPlayerAccount {
      * @return the unique identifier of the player's account as an integer
      */
     int getId();
+
+    /**
+     * Retrieves the universally unique identifier (UUID) of the player's account.
+     * <p>
+     * The UUID is a 128-bit value used to uniquely identify the player's account
+     * across different systems and platforms. Unlike a simple numeric ID, a UUID
+     * provides a high degree of uniqueness and can be used to ensure that the
+     * player's account is globally unique.
+     * </p>
+     * <p>
+     * The UUID is commonly used in scenarios where systems need to integrate or
+     * communicate with each other, ensuring consistent identification of the
+     * player's account without the risk of collision.
+     * </p>
+     *
+     * @return the UUID of the player's account as a {@link java.util.UUID} object
+     */
+    UUID getUUID();
 
     /**
      * Gets the IP address of the player.
