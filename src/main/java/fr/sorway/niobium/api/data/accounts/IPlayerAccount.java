b7ed07b4c544a225e2085b247d60a856d8d57b9a
Sorway <sorwayhd@gmail.com>
2024-06-21 23:10:51 +0200

    📚 Adding JavaDocs

--- src/main/java/fr/sorway/niobium/api/data/accounts/IPlayerAccount.java
+++ src/main/java/fr/sorway/niobium/api/data/accounts/IPlayerAccount.java
@@ -5,6 +5,18 @@ import fr.sorway.niobium.api.data.accounts.data.IPlayerRank;
 public interface IPlayerAccount {
 
     /**
+     * Retrieves the unique identifier of the player's account.
+     * <p>
+     * This ID is typically used to uniquely distinguish the account
+     * in the system. It can be used for various purposes such as
+     * tracking, querying, and managing player accounts.
+     * </p>
+     *
+     * @return the unique identifier of the player's account as an integer
+     */
+    int getId();
+
+    /**
      * Gets the IP address of the player.
      *
      * @return the player's IP address
