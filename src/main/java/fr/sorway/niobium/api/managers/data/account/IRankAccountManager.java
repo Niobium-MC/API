ff852390152ef4e95c35d5b6ef90ed399f2798a4
Sorway <sorwayhd@gmail.com>
2024-06-22 11:08:56 +0200

    📚 Update IRankAccountManager

--- src/main/java/fr/sorway/niobium/api/managers/data/account/IRankAccountManager.java
+++ src/main/java/fr/sorway/niobium/api/managers/data/account/IRankAccountManager.java
@@ -14,14 +14,20 @@ public interface IRankAccountManager {
     void assignRank(IPlayerAccount account, IAccountRank rank);
 
     /**
-     * Removes the specified rank from the given player account.
+     * Adds a rank to the specified player account.
+     *
+     * @param account the player account to which the rank will be added
+     * @param rank the rank to be added to the player's account
+     */
+    void addRank(IPlayerAccount account, IAccountRank rank);
+
+    /**
+     * Removes a specific rank from a player's account.
      *
      * @param account the player account from which the rank will be removed.
-     *                Must not be null.
-     * @param rank the rank to be removed from the player account.
-     *             Must not be null.
+     * @param rank the rank to remove from the player account.
      */
-    void unassignRank(IPlayerAccount account, IAccountRank rank);
+    void removeRank(IPlayerAccount account, IAccountRank rank);
 
     /**
      * Vérifie si le compte du joueur a un rang spécifique.
