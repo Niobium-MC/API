8d9042ebf00fd66d5f414d938ec8acf43ad5895b
Sorway <sorwayhd@gmail.com>
2024-06-23 18:16:58 +0200

    📚 Adding TextColor

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "fr.sorway.niobium"
-version = "1.0-beta.18"
+version = "1.0-beta.19"
 
 repositories {
     mavenCentral()
--- src/main/java/fr/sorway/niobium/api/helpers/TextColors.java
+++ src/main/java/fr/sorway/niobium/api/helpers/TextColors.java
@@ -5,7 +5,7 @@ import org.bukkit.ChatColor;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class TextColor {
+public class TextColors {
 
     private static final Pattern HEX_PATTERN = Pattern.compile("#[a-fA-F0-9]{6}");
 
