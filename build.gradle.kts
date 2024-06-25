7df64c3767001ab631296dec925d41654a457d2b
Sorway <sorwayhd@gmail.com>
2024-06-25 11:13:48 +0200

    📚 Update TextFormatter

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "fr.sorway.niobium"
-version = "1.0-beta.23"
+version = "1.0-beta.24"
 
 repositories {
     mavenCentral()
--- src/main/java/fr/sorway/niobium/api/message/TextFormatter.java
+++ src/main/java/fr/sorway/niobium/api/message/TextFormatter.java
@@ -7,10 +7,19 @@ import org.bukkit.entity.Player;
 
 public class TextFormatter {
 
+    /**
+     * Récupère le nom complet du joueur, incluant son préfixe, son suffixe, et sa couleur de rang.
+     *
+     * @param api L'instance de NiobiumAPI utilisée pour accéder aux données des comptes.
+     * @param player L'instance du joueur dont on veut récupérer le nom complet.
+     * @return Une chaîne de caractères représentant le nom complet du joueur avec son rang.
+     */
     public static String playerName(NiobiumAPI api, Player player) {
-        final IPlayerAccount account = api.getAccountManager().getAccount(player);
-        final IRank rank = account.getPlayerRank().getActiveRank().getRank();
+        IPlayerAccount account = api.getAccountManager().getAccount(player);
+        IRank rank = account.getPlayerRank().getActiveRank().getRank();
 
-        return String.format("%s %s%s %s", rank.getPrefix(), rank.getColor(), player.getName(), rank.getSuffix());
+        return rank.getSuffix() == null
+                ? String.format("%s %s%s", rank.getPrefix(), rank.getColor(), player.getName())
+                : String.format("%s %s%s %s", rank.getPrefix(), rank.getColor(), player.getName(), rank.getSuffix());
     }
 }
